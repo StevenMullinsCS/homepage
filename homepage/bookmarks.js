d09d70366fc2f567b2dfd2f66bf860db0835dbc5
Steven Mullins <100733236+Womboi@users.noreply.github.com>
2022-09-10 00:38:15 -0400

    Add files via upload

---
+++ homepage/bookmarks.js
@@ -0,0 +1,51 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Goldlink", url: "https://goldlink.etsu.edu"},
+      { name: "Outlook", url: "https://mail.outlook.com"},
+      { name: "D2L", url: "https://elearn.etsu.edu/d2l/home"},
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Plex", url: "https://plex.tv" },
+      { name: "East Tennessean", url: "https://easttennessean.com/" },
+      {
+        name: "Amazon Prime", url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/livestreamfail", url: "https://reddit.com/r/livestreamfail" },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      {
+        name: "/r/ETSU",
+        url: "https://reddit.com/r/ETSU",
+      },
+      { name: "/r/hacking", url: "https://reddit.com/r/hacking" },
+      { name: "/r/piracy", url: "https://reddit.com/r/piracy" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "LinkedIn", url: "https://linkedin.com"},
+      { name: "Instagram", url: "https://instagram.com"},
+    ],
+  },
+];
